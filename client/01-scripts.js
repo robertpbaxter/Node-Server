cc686ba06e1348cbeb13053d130b898dbdb00b0a
Robert Baxter <roer.baxter@gmail.com>
2018-09-27 20:47:41 -0400

    9/27/18

--- client/01-scripts.js
+++ client/01-scripts.js
@@ -39,3 +39,24 @@ const postToOneArrow=()=>{
     .catch(error=>console.error('Error:',error))
     .then(response=>console.log('Success:',response))
 }
+
+const postData=()=>{
+    let content={testdata:{item:'This was saved!'}}
+
+    let testDataAfterFetch = document.getElementById('test-data')
+    let createdAtAfterFetch = document.getElementById('created-at')
+
+    fetch('http://localhost:3000/test/seven',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(content)
+    })
+    .then(response=>response.json())
+    .then(function(text){
+        console.log(text)
+        testDataAfterFetch.innerHTML=text.testdata.testdata
+        createdAtAfterFetch.innerHTML=text.testdata.createdAt
+    })
+}
