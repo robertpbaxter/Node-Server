7e0b4f8aec0150f201a4fe4247768a606a974081
Robert <roer.baxter@gmail.com>
2018-09-30 22:03:58 -0400

    9/30/18

---
+++ workoutLog/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express=require('express')
+const app=express()
+const log=require('./controllers/logcontroller')
+const user=require('./controllers/usercontroller')
+const sequelize=require('./db')
+const bodyParser=require('body-parse')
+
+sequelize.sync() //pass in {force:true} for resetting tables
+
+app.use(bodyParser.json())
+app.use(require('./middleware/headers'))
+
+//Exposed routes (create user here)
+app.use('/api',user)
+
+//Protected routes(require login)
+app.use(require('./middleware/validate-session'))
+app.use('/api/log',log)
+
+app.listen(3000,()=>console.log('Server online'))
